3401811a4b75256da6b74fb0b36d33458f924165
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2017-01-30 14:15:04 +0200

    Fix script to retrieve activity log

--- get_git_month_log.py
+++ get_git_month_log.py
@@ -3,7 +3,7 @@
 
 # install https://github.com/PyGithub/PyGithub
 from github import Github
-from datetime import datetime, date
+from datetime import datetime   # , date
 from argparse import ArgumentParser
 from config import GITHUB_USERNAME, GITHUB_PASS, GITHUB_AUTHOR, GITHUB_ORGS
 
@@ -15,25 +15,32 @@ def is_work_repo(url):
     return False
 
 
+def month(d):
+    return (d.year, d.month)
+
+
 def main():
 
     parser = ArgumentParser(description="Retrieve Github activity")
     parser.add_argument('month', type=int, help='Month for activity log')
     args = parser.parse_args()
-    month = args.month
     g = Github(GITHUB_USERNAME, GITHUB_PASS)
     usr = g.get_user(GITHUB_USERNAME)
 
     year = datetime.now().year
-
-    start = date(year=year, month=month, day=1)
+    this = (year, int(args.month))
+    tpl = "{0} - {1}: {2}"
 
     for event in usr.get_public_events():
         # go back in history until the start date is reached
         if event.type != u"PushEvent":
             continue
-        if event.created_at.date() <= start:
+
+        if month(event.created_at.date()) > this:
+            continue
+        if month(event.created_at.date()) < this:
             break
+
         repo_url = event._repo.value.html_url
         if not is_work_repo(repo_url):
             continue
@@ -42,8 +49,10 @@ def main():
         for commit in commits:
             if GITHUB_AUTHOR not in commit['author']['name'].lower():
                 continue
-            print(event.created_at.date(), " - ", repo, ": ",
-                  commit['message'])
+
+            print tpl.format(event.created_at.date(),
+                             repo,
+                             commit['message'])
 
 
 if __name__ == "__main__":
