2b70050bd00c26d36bfdcb933a975014f0fa3293
Zoltan Andras <zoli1884@gmail.com>
2017-02-06 11:43:04 +0200

    Added months and sync arguments

--- gmusic_last_added.py
+++ gmusic_last_added.py
@@ -1,39 +1,92 @@
-#!./bin/python
+#!/bin/env python
 
 from datetime import datetime
 from gmusicapi import Mobileclient
-from config import GOOGLE_EMAIL, GOOGLE_MUSIC_PASS
+from argparse import ArgumentParser
+from tabulate import tabulate
+from config import GOOGLE_EMAIL, GOOGLE_MUSIC_PASS  
+import os.path
+import pprint
+import json
 
+pp = pprint.PrettyPrinter(indent=2)
 
 def resort_by_added(lib):
-    # for track in lib:
-    #     track['creationTimestamp'] = int(track['creationTimestamp'])
-
     return sorted(lib, key=lambda x: x['creationTimestamp'])
 
 
-def format_ts(ts):
+def get_datetime(ts):
+    
     d = datetime.fromtimestamp(float(ts[:10]))
-    return d.strftime("%a %d-%m-%Y %H:%M")
-
+    return d
 
 def main():
-    api = Mobileclient()
-    api.login(GOOGLE_EMAIL,
-              GOOGLE_MUSIC_PASS,
-              Mobileclient.FROM_MAC_ADDRESS)
 
-    library = api.get_all_songs()
+    parser = ArgumentParser(description="Show last added music")
+    parser.add_argument('-m',
+                        '--months',
+                        help='Number of months',
+                        type=int,
+                        default=1)
+    parser.add_argument('-s',
+                        '--sync',
+                        help='Synchronise library with the latest songs',
+                        action="store_true")
+    
+    args = parser.parse_args()
 
-    library = reversed(resort_by_added(library))
+    months = args.months
+    sync = args.sync
 
-    i = 0
-    for track in library:
-        i += 1
-        print track['title'], ' / ', format_ts(track['creationTimestamp'])
-        if i > 20:
-            break
+    if os.path.isfile('songs.json') and not sync:
+        f = open('songs.json', 'r')
+        songs = f.read()
+        songs = json.loads(songs)
+        
+    else:
+        api = Mobileclient()
+        api.login(GOOGLE_EMAIL,
+                  GOOGLE_MUSIC_PASS,
+                  Mobileclient.FROM_MAC_ADDRESS)
 
+        songs_gen = api.get_all_songs(True)
+
+        print "Loading library songs:"
+
+        songs = []
+        for part in songs_gen:
+            songs = songs + part
+            print "%s songs loaded" % len(songs)
+            print "{} songs loaded".format(len(songs))
+
+            str(len(songs)) + " songs loaded."
+
+         songs = list(reversed(resort_by_added(songs)))
+        
+        f = open('songs.json', 'w')
+        f.write(json.dumps(songs, indent=4, separators=(',', ': ')))
+        f.close()
+
+    c = 1
+    flag = True
+    final_list = []
+    for track in songs:
+        
+        d = get_datetime(track['creationTimestamp'])
+        
+        if flag:
+            current_month = d.month
+            flag = False
+
+        if current_month != d.month:
+            c += 1
+            if c > months:
+                break
+
+        current_month = d.month
+        final_list = final_list + [[ track['title'], d.strftime("%a %d-%m-%Y %H:%M") ]]
+    
+    print tabulate(final_list, headers=['Song', 'Date'])
 
 if __name__ == "__main__":
     main()
